a61f891ffe9189c8d0ed2c2c606da78630021a56
@tatianaquiroga <tatianaquiroga81@gmail.com>
2022-04-23 20:36:48 -0300

    commit init

--- js/lastmodi.js
+++ js/lastmodi.js
@@ -1,9 +1,9 @@
-// fileInput es un HTMLInputElement: <input type="file" multiple id="myfileinput">
-var fileInput = document.getElementById("myfileinput");
+const footerYear = document.querySelectorAll(".year");
+footerYear.forEach(copyright => {
+    copyright.innerHTML = new Date().getFullYear();
+});
 
-// files es un objeto FileList (similar a NodeList)
-var files = fileInput.files;
-
-for (var i = 0; i < files.length; i++) {
-  alert(files[i].name + " tiene una fecha de ultima modificacion el " + files[i].lastModifiedDate);
-}
+const date = document.querySelectorAll(".date");
+date.forEach(modify => {
+    modify.innerHTML = new Date(document.lastModified);
+});
